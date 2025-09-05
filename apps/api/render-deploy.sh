585dfcc35203487352b78768643299cd4b60abaf
Prakash Austin Heramil <ph@Prakashs-MacBook-Pro.local>
2025-09-04 18:58:46 -0700

    deploy: runnable render-deploy.sh and Dockerfile tweaks

--- apps/api/render-deploy.sh
+++ apps/api/render-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Composer optimize..."
-composer install --no-dev --prefer-dist --no-interaction
+composer install --no-dev --prefer-dist --no-interaction || true
 
 php artisan key:generate --force || true
 php artisan config:cache
